3d86b09d68abcbefa0f3392855508c67ffe1b505
SheenIsQueen <afsheen.jamal@ryerson.ca>
2021-03-21 14:25:57 -0400

    Started spiraling, but bit better.

--- Game/GameEng.java
+++ Game/GameEng.java
@@ -6,12 +6,14 @@ import javax.swing.*;
 
 public class GameEng extends JFrame{
 
-    private JPanel panel = new JPanel();
-    private JTextArea txtArea = new JTextArea();
+    private JPanel panel = new JPanel(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+    private Scanner in = new Scanner(System.in);
+    private JTextArea txtArea = new JTextArea(10, 10);
+    private JTextField txtField = new JTextField(10);
     private boolean isRunning = false;
     private int windowWidth = 940, windowHeight = 640;
     private long fps = 6;
-    private int x = 0;
 
     public static void main(String[] args){
         GameEng gc = new GameEng();
@@ -48,12 +50,12 @@ public class GameEng extends JFrame{
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
-        add(txtArea);
+        add(panel);
     }
 
     //will check input and update any parameters
     void update(){
-        x++;
+
     }
 
     //will send the respective text
@@ -66,9 +68,22 @@ public class GameEng extends JFrame{
         }
         txtArea.setText(fileContent);
         txtArea.setEditable(false);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.anchor = GridBagConstraints.PAGE_START;
+        panel.add(txtArea, c);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 40;
+        c.weightx = 0.0;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 1;
+        panel.add(txtField, c);
         fileIn.close();
         }catch (FileNotFoundException e){
             System.out.println("Invalid Input.");
         }
     }
+
 }
