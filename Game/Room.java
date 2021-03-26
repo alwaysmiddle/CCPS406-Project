6c6214479591dd6986127d685d6ff9378d5fbc0b
JRB2015 <johnrogersbradley@gmail.com>
2021-03-26 12:38:57 -0400

    sample room data

--- Game/Room.java
+++ Game/Room.java
@@ -1,17 +1,37 @@
 import java.io.*;
 import java.util.*;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Room {
 
     //max number of rooms in house
     public static final int ROOMS = 6;
     public static final int FEATURES = 4;
 
-    //array to hold all of the rooms
+    //array to hold all of the rooms and room details
     public static Room[][] house = new Room[ROOMS][FEATURES];
 
     private Room(){
         JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader("room.json"));
+        {
+            //read JSON file into JSON array
+            Object obj = jsonParser.parse(reader);
+            JSONArray roomList = (JSONArray) obj;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
     //provides long description of room
