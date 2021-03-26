b496a3fb3048b28499f1a5428687711d2ff8ce23
JRB2015 <johnrogersbradley@gmail.com>
2021-03-26 12:21:21 -0400

    sample room data

--- Game/Room.java
+++ Game/Room.java
@@ -4,11 +4,15 @@ import java.util.*;
 public class Room {
 
     //max number of rooms in house
-    public static final int ROOMS = 9;
-    public static final int FEATURES = 8;
+    public static final int ROOMS = 6;
+    public static final int FEATURES = 4;
 
     //array to hold all of the rooms
     public static Room[][] house = new Room[ROOMS][FEATURES];
+
+    private Room(){
+        JSONParser jsonParser = new JSONParser();
+    }
 
     //provides long description of room
     private String longDescription(){
