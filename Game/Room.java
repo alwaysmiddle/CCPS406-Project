b63e83a5733751efa75c1d8cc56f8071f2fe6e07
JRB2015 <johnrogersbradley@gmail.com>
2021-03-26 20:48:59 -0400

    sample room data

--- Game/Room.java
+++ Game/Room.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -7,56 +6,53 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Room {
-
     //max number of rooms in house
     public static final int ROOMS = 6;
-    public static final int FEATURES = 4;
+    public static final int FEATURES = 6;
+    int room = 0;
 
     //array to hold all of the rooms and room details
-    public static Room[][] house = new Room[ROOMS][FEATURES];
+    public static String[][] house = new String[ROOMS][FEATURES];
 
+    //loads starting state room info into array
     private Room(){
         JSONParser jsonParser = new JSONParser();
 
-        try (FileReader reader = new FileReader("room.json"));
+        try (FileReader reader = new FileReader("room.json"))
         {
             //read JSON file into JSON array
             Object obj = jsonParser.parse(reader);
             JSONArray roomList = (JSONArray) obj;
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
-
-    }
-
-    //provides long description of room
-    private String longDescription(){
-        String longDes = "long";
-
-        return longDes;
-    }
-
-    //provides short description of rooms
-    private String shortDescription(){
-        String shortDes = "short";
-
-        return shortDes;
     }
 
     //indicates whether a room has been visited or not
     public boolean visited(){
+        if (house[room][1] == "no") {
+            house[room][1] = "yes";
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 
-        return true;
+    //provides short description of rooms
+    private String shortDescription(){
+        return house[room][2];
+    }
+
+    //provides long description of room
+    private String longDescription(){
+        return house[room][3];
     }
 
     //determine which description to load
     public String loadDescription(Boolean visited){
-        if (visited = false){
+        if (!visited()){
             return longDescription();
         }
         else {
