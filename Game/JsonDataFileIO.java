c317cdc7dca8668ab945a3dd0b0a162dab39d8b8
Peter YSZ <ysz10000@hotmail.com>
2021-04-03 03:25:44 -0400

    Update from Peter, Added a singleton class for read/write JSON files

--- Game/JsonDataFileIO.java
+++ Game/JsonDataFileIO.java
@@ -1,9 +1,29 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import java.io.FileReader;
+import java.io.IOException;
 
 //singleton object only 1 instance exists per game.
 public class JsonDataFileIO {
     private static JsonDataFileIO jsonIoInstance = null;
-    public Object ReadFile(){
-        return null;
+    private JSONParser jsonParser = new JSONParser();
+
+    public Object ReadFile(String filePath){
+        Object obj;
+
+        try (FileReader reader = new FileReader(filePath))
+        {
+            //read JSON file into JSON array
+            obj = jsonParser.parse(reader);
+
+            return obj;
+
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     public void WriteToJsonFile(Object fileToWrite)
