f06c7be4ef8c425a7fde4176917e18da0be01bee
Peter YSZ <ysz10000@hotmail.com>
2021-04-04 18:49:58 -0400

    Update from Peter, Added a singleton class for read/write JSON files

--- Game/JsonDataFileIO.java
+++ Game/JsonDataFileIO.java
@@ -5,33 +5,25 @@ import com.google.gson.stream.JsonReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 
 //singleton object only 1 instance exists per game.
 public class JsonDataFileIO {
     private static JsonDataFileIO singletonInstance = null;
-    private JsonReader reader;
 
-    public <T> List<T> ReadFile(String filePath){
+    public static <T> List<T> ReadFile(TypeToken<List<T>> fileType, String filePath){
         Gson gson = new Gson();
 
         try (FileReader reader = new FileReader(filePath))
         {
-            Type typePassed = new TypeToken<List<T>>(){}.getType();
-
-            //read JSON file into a list
-            List<T> readList = gson.fromJson(reader, typePassed);
-            return readList;
-
+            return gson.fromJson(reader, fileType.getType());
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public <T> void WriteToJsonFile(List<T> objToWrite, String filePath)
+    public static <T> void WriteToJsonFile(List<T> objToWrite, String filePath)
     {
         Gson gson = new Gson();
         //write file to game folder here
--- Game/JsonDataObjList.java
+++ Game/JsonDataObjList.java
@@ -1,8 +1,3 @@
-import com.google.gson.Gson;
-import com.google.gson.stream.JsonReader;
-
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,54 +6,44 @@ public class JsonDataObjList {
     private static List<Room> _listOfRooms = new ArrayList<Room>();
     private static List<Item> _listOfItems = new ArrayList<Item>();
     private static JsonDataObjList singletonInstance = null;
-    private JsonReader reader;
 
     //constructor
     public JsonDataObjList() {
         //load the list of json objects into public fields for rest of the program to work with
+        _listOfRooms = JsonDataFileIO.getInstance().ReadFile(GlobalReference.ROOM_JSON_FILE_LOCATION);
 
     }
 
-
-    public Object ReadFile(String filePath){
-        Object obj;
-
-        try (FileReader reader = new FileReader(filePath))
-        {
-            //read JSON file into JSON array
-            obj = jsonParser.parse(reader);
-            return obj;
-
-        } catch (ParseException | IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public void SaveToJsonFile()
+    public void Save()
     {
-        Gson gson = new Gson();
-        //write file to game folder here
+        //save the list of rooms
+        //save the list of items
+        //save the player status
 
-        String jsonToWrite = gson.toJson(_listOfItems);
-
-        try (FileWriter writer = new FileWriter(GlobalReference.ITEM_JSON_FILE_LOCATION)) {
-            gson.toJson()
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public Room getSingleRoom(String roomName){
+
         return null;
     }
 
-    public List<Room> getListOfRooms(){
-        return _listOfRooms;
+    public Room getSingleItem(String roomName){
+
+        return null;
     }
 
-    public List<Room> getListOfItems(){
+
+    public List<Room> getRoomsHashmap() {
+        //return hashmap
+        return _listOfRooms;
+
+    }
+
+    public List<Room> getItemsHashmap()
+    {
+        //return hashmap
         return _listOfItems;
+
     }
 
 
