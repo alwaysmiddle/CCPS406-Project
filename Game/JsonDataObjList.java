42768befd2eb2c2a9264fd851033f8c5491ecec8
Peter YSZ <ysz10000@hotmail.com>
2021-04-05 00:36:37 -0400

    Update from Peter

--- Game/JsonDataObjList.java
+++ Game/JsonDataObjList.java
@@ -5,7 +5,11 @@ public class JsonDataObjList {
     //private fields
     private static Map<String, Room> _mapOfRooms = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     private static Map<String, Item> _mapOfItems = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-    private static PlayerStatus _player = new PlayerStatus();
+    private static PlayerStatus _player;
+    private List<Room> _listOfRooms;
+    private List<Item> _listOfItems;
+    private List<PlayerStatus> _players;
+
     private static JsonDataObjList singletonInstance = null;
 
     //constructor
@@ -13,19 +17,19 @@ public class JsonDataObjList {
         //load the list of json objects into public fields for rest of the program to work with
 
         //rooms
-        List<Room> _listOfRooms = JsonDataFileIO.getInstance().readJsonFile(new TypeToken<List<Room>>(){}.getType(), GlobalReference.ROOM_JSON_FILE_LOCATION);
+        _listOfRooms = JsonDataFileIO.getInstance().readJsonFile(new TypeToken<List<Room>>(){}.getType(), GlobalReference.ROOM_JSON_FILE_LOCATION);
         for (Room r: _listOfRooms) {
             _mapOfRooms.put(r.getRoomName().toLowerCase(), r);
         }
 
         //items
-        List<Item> _listOfItems = JsonDataFileIO.getInstance().readJsonFile(new TypeToken<List<Item>>(){}.getType(), GlobalReference.ITEM_JSON_FILE_LOCATION);
+        _listOfItems = JsonDataFileIO.getInstance().readJsonFile(new TypeToken<List<Item>>(){}.getType(), GlobalReference.ITEM_JSON_FILE_LOCATION);
         for (Item t: _listOfItems) {
             _mapOfItems.put(t.getItemName().toLowerCase(), t);
         }
 
         //Player status
-        List<PlayerStatus> _players = JsonDataFileIO.getInstance().readJsonFile(new TypeToken<List<PlayerStatus>>(){}.getType(), GlobalReference.PLAYER_STATUS_FILE_LOCATION);
+        _players = JsonDataFileIO.getInstance().readJsonFile(new TypeToken<List<PlayerStatus>>(){}.getType(), GlobalReference.PLAYER_STATUS_FILE_LOCATION);
         _player = _players.get(0);
 
     }
@@ -33,8 +37,13 @@ public class JsonDataObjList {
     public void Save()
     {
         //save the list of rooms
+        JsonDataFileIO.writeJsonFile(_listOfRooms, GlobalReference.ROOM_JSON_FILE_LOCATION);
+
         //save the list of items
+        JsonDataFileIO.writeJsonFile(_listOfItems, GlobalReference.PLAYER_STATUS_FILE_LOCATION);
+
         //save the player status
+        JsonDataFileIO.writeJsonFile(_players, GlobalReference.PLAYER_STATUS_FILE_LOCATION);
     }
 
     //map format is: <room_name, room_object>, getSingle will try to match the input with key and return object
